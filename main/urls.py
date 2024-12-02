dbecb95aee2af402bf1b3479e95f0aac2b096a9f
?0 <benjamindguntara@protonmail.com>
2024-12-02 18:58:47 +0700

    ojsodifdsofj

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", index, name="index"),
     path('login/', login, name="login"),
     path('signup/', signup, name="signup"),
-    path("player-lists/", playerlist,name="player"),
+    path("teams/", teams,name="team"),
     path("scoreboard/", scoreboard,name="scoreboard"),
     path("challenge/", challenge,name="challenge"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,11 +4,23 @@ from django.shortcuts import render
 def index(request):
     return render(request, "pages/main/index.html")
 
-def playerlist(request):
-    return render(request, "pages/main/playerlists.html")
+def teams(request):
+    return render(request, "pages/main/teams.html")
 
 def scoreboard(request):
-    return render(request, "pages/main/scoreboard.html")
+    score = [
+      { "y": 100, "label": "Texas" },
+      { "y": 200, "label": "Pennsylvania" },
+      { "y": 200, "label": "Florida" },
+      { "y": 100, "label": "New York" },
+      { "y": 250, "label": "Massachusetts" },
+      { "y": 100, "label": "Texas" },
+      { "y": 200, "label": "Pennsylvania" },
+      { "y": 200, "label": "Florida" },
+      { "y": 100, "label": "New York" },
+      { "y": 250, "label": "Massachusetts" },
+    ]
+    return render(request, "pages/main/scoreboard.html", context={"scoreboard":score})
 
 def challenge(request):
     return render(request, "pages/main/challenge.html")
