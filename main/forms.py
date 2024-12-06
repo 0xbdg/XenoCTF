3e2e5fa183a3c604a33ce3ddc80dce5fbf41452e
?0 <benjamindguntara@protonmail.com>
2024-12-06 15:50:04 +0700

    ooijoijoijoij

--- main/forms.py
+++ main/forms.py
@@ -1,1 +1,17 @@
 from django import forms
+from django.forms.widgets import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+class LoginForm(forms.Form):
+    username=forms.CharField()
+    password=forms.CharField()
+
+class SignupForm(UserCreationForm):
+    username=forms.CharField()
+    email=forms.EmailField()
+    password1=forms.CharField()
+    password2=forms.CharField()
+
+    class Meta:
+        model = User
